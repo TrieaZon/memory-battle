5466f1d04f2bbe7d41e4c06ad95e926374e21517
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2024-01-09 16:46:41 -0800

    edits

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="opacity-95 flex min-h-screen flex-col items-center justify-between p-5">
         <div className="z-10 my-5 mx-5 max-w-4xl w-full items-center justify-center font-mono text-sm">
           <h1 className="fixed left-0 top-0 flex w-full justify-center text-4xl border-b border-slate-400 bg-gradient-to-b from-zinc-200 pb-6 pt-16 backdrop-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
-            Cool font for "Memory Battle" title
+            Cool font for Memory Battle title
           </h1>
         </div>
         
